7fdba2b0167ca0bb873c1d746e5fc886fc294a60
yuralala8 <yc1435@nyu.edu>
2017-06-23 21:35:16 -0400

    programming_languages

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
-  # your code here
-end
+    hash = {}
+    
+    languages.each do |style, data|
+        data.each do |lang, type_value|
+            if hash[lang] == nil
+                hash[lang] = type_value
+                hash[lang][:style] = [style]
+            else
+            hash[lang][:style] << style
+            end
+        end
+    end
+    hash
+  end
+
